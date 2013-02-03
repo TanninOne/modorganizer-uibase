55abba05dae960e93729548a47e0192d0e0c3bde
Tannin <sherb@gmx.net>
2013-02-03 13:34:53 +0100

    bsatk is now part of the Mod Organizer repository

--- src/diagnosisreport.cpp
+++ src/diagnosisreport.cpp
@@ -1,20 +1,21 @@
 /*
+Mod Organizer shared UI functionality
+
 Copyright (C) 2012 Sebastian Herbord. All rights reserved.
 
-This file is part of Mod Organizer.
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
 
-Mod Organizer is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Mod Organizer is distributed in the hope that it will be useful,
+This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
-along with Mod Organizer.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "diagnosisreport.h"
