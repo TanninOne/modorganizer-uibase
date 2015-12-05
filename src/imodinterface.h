e059c404455b863589259ae6d1776b89dc0d696a
Thomas Tanner <trtanner@btinternet.com>
2015-12-05 10:42:23 +0000

    Missing forward refs

--- src/imodinterface.h
+++ src/imodinterface.h
@@ -22,6 +22,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef IMODINTERFACE_H
 #define IMODINTERFACE_H
 
+class QString;
+class QStringList;
 
 namespace MOBase {
 
